ccfccd8d42a1e5c5e40e028330f516e4130479a9
Acellam Guy <abiccel@yahoo.com>
2017-07-31 08:12:13 +0300

    Make alert generic

--- src/components/Alert.ts
+++ src/components/Alert.ts
@@ -1,9 +1,10 @@
 import { SFC, createElement } from "react";
+import * as classNames from "classnames";
 
 type bootstrapStyle = "danger" | "info" | "success" | "inverse" | "warning";
-export const Alert: SFC<{ bootstrapStyle: bootstrapStyle, message?: string }> = (props) =>
+export const Alert: SFC<{ bootstrapStyle: bootstrapStyle, message?: string, className?: string }> = (props) =>
     props.message
-        ? createElement("div", { className: `alert alert-${props.bootstrapStyle}` }, props.message)
+        ? createElement("div", { className: classNames(`alert alert-${props.bootstrapStyle}`, props.className) }, props.message)
         : null;
 
 Alert.displayName = "Alert";
