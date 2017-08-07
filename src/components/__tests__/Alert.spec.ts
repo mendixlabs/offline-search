d465dc204d960703a11681c3647211ce3d09557e
Acellam Guy <abiccel@yahoo.com>
2017-08-07 14:19:53 +0300

    Add initial unit test spec

---
+++ src/components/__tests__/Alert.spec.ts
@@ -0,0 +1,14 @@
+describe("Alert", () => {
+
+    it("renders structure correctly", () => {
+        //
+    });
+
+    it("renders no structure when the alert message is not specified", () => {
+        //
+    });
+
+    it("shows green when bootstrap style is success", () => {
+        //
+    });
+});
---
+++ src/components/__tests__/SearchBar.spec.ts
@@ -0,0 +1,32 @@
+describe("SearchBar", () => {
+
+    it("renders structure correctly", () => {
+        //
+    });
+
+    it("is not visible when show appearance is set to no", () => {
+        //
+    });
+
+    it("shows place holder when specified", () => {
+        //
+    });
+
+    describe("input", () => {
+        it("accepts value", () => {
+            //
+        });
+
+        it("updates when the search changes", () => {
+            //
+        });
+
+        it("has input when default query is specified", () => {
+            //
+        });
+
+        it("is cleared when the remove button is clicked", () => {
+            //
+        });
+    });
+});
