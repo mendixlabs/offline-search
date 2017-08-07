4330d5ce0b3186200f659f94321c1cb3517e6cad
Acellam Guy <abiccel@yahoo.com>
2017-08-07 17:40:08 +0300

    Add alert tests

--- src/components/__tests__/Alert.spec.ts
+++ src/components/__tests__/Alert.spec.ts
@@ -1,14 +1,40 @@
+import { createElement } from "react";
+import { shallow } from "enzyme";
+import { Alert } from "../Alert";
+
 describe("Alert", () => {
 
     it("renders structure correctly", () => {
-        //
+        const message = "This is an error";
+        const alert = shallow(createElement(Alert, { bootstrapStyle: "danger", message }));
+
+        expect(alert).toBeElement(
+            createElement("div", { className: "alert alert-danger" }, message)
+        );
     });
 
     it("renders no structure when the alert message is not specified", () => {
-        //
+        const alert = shallow(createElement(Alert, { bootstrapStyle: "danger" }));
+
+        expect(alert).toBeElement(null);
     });
 
     it("shows green when bootstrap style is success", () => {
-        //
+        const message = "This is an error";
+        const alert = shallow(createElement(Alert, { bootstrapStyle: "success", message }));
+
+        expect(alert).toBeElement(
+            createElement("div", { className: "alert alert-success" }, message)
+        );
+    });
+
+    it("renders class name correctly", () => {
+        const message = "This is an error";
+        const className = "widget-offline-search";
+        const alert = shallow(createElement(Alert, { bootstrapStyle: "danger", message, className }));
+
+        expect(alert).toBeElement(
+            createElement("div", { className: "alert alert-danger widget-offline-search" }, message)
+        );
     });
 });
