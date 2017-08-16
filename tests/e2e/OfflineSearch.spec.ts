c9bea06a9183741304991bc4d3ab9ba4df07d60c
Acellam Guy <abiccel@yahoo.com>
2017-08-16 16:22:31 +0300

    Add e2e tests

--- tests/e2e/OfflineSearch.spec.ts
+++ tests/e2e/OfflineSearch.spec.ts
@@ -1,7 +1,40 @@
-import HomePage from "./pages/home.page";
+import page from "./pages/home.page";
+import { Element } from "webdriverio";
+
+const testValue = "yellow";
 
 describe("OfflineSearch", () => {
-    it("when search is entered list view should be filtered", () => {
-        HomePage.open();
+    it("when query is entered list view should be filtered", () => {
+        page.open();
+        page.searchInput.waitForVisible();
+        page.searchInput.click();
+        page.searchInput.setValue(testValue);
+
+        const listviewItems: Element[] = page.listViewList.value;
+        expect(listviewItems.length).toBe(1);
+    });
+
+    it("when query is entered and cleared list view should be filtered", () => {
+        page.open();
+        page.searchInput.waitForVisible();
+        page.searchInput.click();
+        page.searchInput.setValue(testValue);
+
+        let listviewItems: Element[] = page.listViewList.value;
+        expect(listviewItems.length).toBe(1);
+
+        page.searchButton.click();
+
+        setTimeout(() => {
+            listviewItems = page.listViewList.value;
+            expect(listviewItems.length).toBeGreaterThan(1);
+        }, 3000);
+
+        page.searchInput.setValue("e");
+
+        setTimeout(() => {
+            listviewItems = page.listViewList.value;
+            expect(listviewItems.length).toBeGreaterThan(2);
+        }, 3000);
     });
 });
--- tests/e2e/pages/home.page.ts
+++ tests/e2e/pages/home.page.ts
@@ -1,4 +1,11 @@
+import { Client, Element, RawResult } from "webdriverio";
+
 class HomePage {
+    public get searchInput() { return browser.element(".mx-name-offlineSearch2 input"); }
+    public get searchButton() { return browser.element(".mx-name-offlineSearch2 button"); }
+    public get listViewList(): Client<RawResult<Element[]>> & RawResult<Element[]> {
+        return browser.elements(".mx-name-listView1 .mx-listview-list");
+    }
 
     public open(): void {
         browser.url("/");
